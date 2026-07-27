d381e47ccc86cc7f8cc5a9e8b86a1c277ff29cef
Bismarkwood <126250596+Bismarkwood@users.noreply.github.com>
2026-07-27 22:13:57 +0000

    feat: update homepage hero background image

--- src/components/HeroSection/heroSlides.ts
+++ src/components/HeroSection/heroSlides.ts
@@ -3,28 +3,13 @@ export interface HeroSlide {
   circleImage: string
 }
 
-import heroImg1 from '../../assets/hero/Image 1.webp'
-import heroImg2 from '../../assets/hero/Image 3.webp'
-import heroImg3 from '../../assets/hero/432324-Korean_Peninsula_node_full_image_2.webp'
-import heroImg4 from '../../assets/hero/Geospatial.webp'
+import heroImg1 from '../../assets/homepage-hero-bg.jpg'
 
 const heroSlides: HeroSlide[] = [
   {
     backgroundImage: heroImg1,
     circleImage: heroImg1,
-  },
-  {
-    backgroundImage: heroImg2,
-    circleImage: heroImg2,
-  },
-  {
-    backgroundImage: heroImg3,
-    circleImage: heroImg3,
-  },
-  {
-    backgroundImage: heroImg4,
-    circleImage: heroImg4,
-  },
+  }
 ]
 
 export default heroSlides
